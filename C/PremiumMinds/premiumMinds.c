f301765f862ea4b335bca388b391be855d47afd7
AndreTeixera <andre.r.teixeira@tecnico.ulisboa.pt>
2022-03-22 00:40:36 +0000

    start string processing

--- C/PremiumMinds/premiumMinds.c
+++ C/PremiumMinds/premiumMinds.c
@@ -4,6 +4,8 @@
 
 // Definition of function do here to make sure there are none undifined references
 void read(int *number, char *string, int *arrayControler, int *stringLenght);
+int getLetterValue(char letter);
+void processDate(char *string, int *arrayControler, int *stringLenght);
 
 /**
  * Stages of the program
@@ -27,6 +29,13 @@ void read(int *number, char *string, int *arrayControler, int *stringLenght)
     scanf("%ms", &string); // Reads a string of unknow size -> this memory is allocated int the heap and will need to be cleaned up
 
     sscanf(string, "%d", &*number); // Gets the number that is contained in the string
+
+    if (*(number) == 0)
+    {                // There was no letter in the array -> the output sequence will be none
+        printf(" "); // Writes the sequence output
+        exit(0);     // leaves the program
+    }
+
     aux1 = *number;                 // this variable is just going to be used in the next loop in order to calculate the number of digits the int "*number" has
     *stringLenght = strlen(string); // Get the string lenght and stores it so that we cont have to calculate it later
 
@@ -37,6 +46,41 @@ void read(int *number, char *string, int *arrayControler, int *stringLenght)
     } while (aux1 != 0);     // Gets the number of digits the the variable *number contains
 
     return; // Makes sure we exit the function correctly
+}
+/**
+ * @brief Get the Letter Value object
+ *
+ * @param letter -> Character that wants its value
+ * @return int -> value of the letter
+ */
+int getLetterValue(char letter)
+{
+    switch (letter) // Switch case with the values of the letters
+    {
+    case 'F':
+        return -1;
+        break;
+    case 'W':
+        return 1;
+        break;
+    case 'E':
+        return 2;
+        break;
+    case 'A':
+        return 2;
+        break;
+    case 'N':
+        return 0;
+        break;
+    default:
+        printf("There was a wrong letter in the array :/");
+        exit(0);
+    }
+}
+
+void processDate(char *string, int *arrayControler, int *stringLenght)
+{
+    int flag = 1;
 }
 
 /**
